dabc4ecd06fb2e027928a0c81b422106d245d743
Samaria44 <samariatajamul563@gmail.com>
2026-05-21 14:05:50 +0500

    .....

--- backend/node-boiler-plate-with-express-main/src/config/db.js
+++ backend/node-boiler-plate-with-express-main/src/config/db.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-module.exports = {
-  HOST: "localhost",
-  PORT: 27017,
-  DB: "app_db"
-};
+// module.exports = {
+//   HOST: "localhost",
+//   PORT: 27017,
+//   DB: "app_db"
+// };
+
+// mongodb+srv://<db_username>:<db_password>@cluster0.wako6dj.mongodb.net/
